b23f4c226138b7428ad79e36141873fa3d6d626b
Shawn Toubeau <shawntoubeau@gmail.com>
2022-02-25 21:58:10 -0500

    feat: Adds test square for movement

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod lane;
 
 use bevy::{prelude::*, window::WindowResizeConstraints};
 use bevy_prototype_lyon::prelude::*;
+use bevy::input::keyboard::KeyboardInput;
+use heron::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 
 const SCREEN_WIDTH: f32 = 1500.;
 const SCREEN_HEIGHT: f32 = 1100.;
@@ -21,12 +24,65 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(PhysicsPlugin::default())
         .add_plugin(ShapePlugin)
-        .add_startup_system(init)
         .add_startup_system(lane::lane_array_builder)
+        .add_startup_system(setup_system)
+        .add_system(keyboard_events)
         .run()
 }
 
-fn init(mut c: Commands) {
-    c.spawn_bundle(OrthographicCameraBundle::new_2d());
+#[derive(Component)]
+struct ExampleShape;
+
+fn setup_system(mut commands: Commands) {
+    let shape = shapes::RegularPolygon {
+        sides: 4,
+        feature: shapes::RegularPolygonFeature::Radius(200.0),
+        ..shapes::RegularPolygon::default()
+    };
+
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(GeometryBuilder::build_as(
+            &shape,
+            DrawMode::Outlined {
+                fill_mode: FillMode::color(Color::CYAN),
+                outline_mode: StrokeMode::new(Color::BLACK, 10.0),
+            },
+            Transform::default(),
+        ))
+        .insert(ExampleShape)
+        // Make it a rigid body
+        .insert(RigidBody::Dynamic)
+
+        // Attach a collision shape
+        .insert(CollisionShape::Sphere { radius: 10.0 })
+
+        // Optionally add other useful components...
+        .insert(Velocity::from_linear(Vec3::X * 2.0))
+        .insert(Acceleration::from_linear(Vec3::X * 1.0))
+        .insert(PhysicMaterial { friction: 1.0, density: 10.0, ..Default::default() })
+        .insert(RotationConstraints::lock());
+}
+
+fn keyboard_events(
+    mut key_evr: EventReader<KeyboardInput>,
+) {
+    use bevy::input::ElementState;
+
+    for ev in key_evr.iter() {
+        match ev.state {
+            ElementState::Pressed => {
+                println!("Key press: {:?} ({})", ev.key_code, ev.scan_code);
+                if ev.key_code == Some(bevy::input::keyboard::KeyCode::W) {
+                    // W is being held down
+                    println!("Up");
+                }
+            }
+            ElementState::Released => {
+                println!("Key release: {:?} ({})", ev.key_code, ev.scan_code);
+            }
+        }
+    }
 }
